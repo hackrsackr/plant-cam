660d0ca85be5cec0d08a6d8d85484c10e58ba439
rob <rob.knighton10@gmail.com>
2024-12-29 07:53:25 -0500

    working on flask page

--- plant_cam/src/app.py
+++ plant_cam/src/app.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template, request, jsonify
+# app.py
+ 
+from flask import Flask, render_template, request, redirect, session
 
-app = Flask(__name__, template_folder="templates")
+ 
+app = Flask(__name__)
+ 
+# dictionary to store user and password
+context = {
+    'photos': 2,
+    'delay': 30,
+    'fps': 12,
+    'start_time': "08:00"
+}
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+# To render a login form 
+@app.route('/')
+def view_form():
+    return render_template('index.html', context=context)
+ 
+# For handling get request form we can get
+# the form inputs value by using args attribute.
+# this values after submitting you will see in the urls.
+# e.g http://127.0.0.1:5000/handle_get?username=kunal&password=1234
+# this exploits our credentials so that's 
+# why developers prefer POST request.
+@app.route('/handle_get', methods=['GET'])
+def handle_get():
+    if request.method == 'GET':
+        # response = {}
+        # response['photos'] = request.args['photos']
+        # response['delay'] = request.args['delay']
+        photos = request.args['photos']
+        delay = request.args['delay']
+        # response['fps'] = request.args['fps']
+        # response['start_time'] = request.args['start_time']
+        print(photos, delay)
+        # context.update(response)
+        # print(context)
+        return render_template('index.html', context=context)
 
-@app.route('/process', methods=['POST'])
-def process():
-    data = request.get_json()
-    number1 = int(data['value']) * 2
-    return jsonify(result=number1)
 
 if __name__ == '__main__':
     app.run(debug=True)
