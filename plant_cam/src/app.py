57b93bdb808f4aa732a1a3032ebfd85d03aa8d98
rob <rob.knighton10@gmail.com>
2024-12-30 16:00:08 -0500

    cleaned up main functionality and got rid of slider

--- plant_cam/src/app.py
+++ plant_cam/src/app.py
@@ -1,53 +1,43 @@
 # app.py
 import json
+
 from flask import Flask, render_template, request 
 
 app = Flask(__name__)
  
 with open('config.json', 'r') as f:
     cfg =json.load(f)
-
-context = dict(cfg['public'])
-
-
-# context = {
-    # 'number_of_photos': cfg['public']['number_of_photos'],
-    # 'delay': cfg['public']['secs_between_photos'],
-    # 'fps': cfg['public']['frames_per_second'],
-    # 'start_time': cfg['public']['start_time']
+    context = dict(cfg['public'])
 
 @app.route('/')
 def view_form():
-    return render_template('index.html', context=context, bg_class='classy')
- 
-@app.route('/reset_config', methods=['GET'])
-def reset():
-    if request.method == 'GET':
-        context = dict(cfg['public'])
-    
-    print(f"context: {context}")
-    return render_template('index.html', context=context, bg_class='classy')
-
-
-@app.route('/handle_get', methods=['GET'])
-def handle_get():
-    if request.method == 'GET':
-        for key in context:
-            if request.args.get(key):
-                context[key] = request.args.get(key)
-                
-    print(f"context: {context}")
     return render_template('index.html', context=context)
-    
-@app.route('/handle_post', methods=['POST'])
-def handle_post():
+
+
+@app.route('/handle_request', methods=['GET', 'POST'])
+def handle_request():
     if request.method == 'POST':
         for key in context:
             if request.form[key]:
                 context[key] = request.form[key]
 
+    if request.method == 'GET':
+        for key in context:
+            if request.args.get(key):
+                context[key] = request.args.get(key)
+
     print(f"context: {context}")
     return render_template('index.html', context=context)
+
+
+@app.route('/reset_config', methods=['POST'])
+def reset():
+    if request.method == 'POST':
+        context = dict(cfg['public'])
+    
+    print(f"context: {context}")
+    return render_template('index.html', context=context)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
