91ec6824f00c3cc1a05016bd5c7cf7f98708ae98
rob <rob.knighton10@gmail.com>
2024-12-30 16:05:34 -0500

    settings

--- plant_cam/src/app.py
+++ plant_cam/src/app.py
@@ -1,43 +1,44 @@
 # app.py
 import json
 
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
- 
-with open('config.json', 'r') as f:
-    cfg =json.load(f)
-    context = dict(cfg['public'])
 
-@app.route('/')
+with open("config.json", "r") as f:
+    cfg = json.load(f)
+    context = dict(cfg["public"])
+
+
+@app.route("/")
 def view_form():
-    return render_template('index.html', context=context)
+    return render_template("index.html", context=context)
 
 
-@app.route('/handle_request', methods=['GET', 'POST'])
+@app.route("/handle_request", methods=["GET", "POST"])
 def handle_request():
-    if request.method == 'POST':
+    if request.method == "POST":
         for key in context:
             if request.form[key]:
                 context[key] = request.form[key]
 
-    if request.method == 'GET':
+    if request.method == "GET":
         for key in context:
             if request.args.get(key):
                 context[key] = request.args.get(key)
 
     print(f"context: {context}")
-    return render_template('index.html', context=context)
+    return render_template("index.html", context=context)
 
 
-@app.route('/reset_config', methods=['POST'])
+@app.route("/reset_config", methods=["POST"])
 def reset():
-    if request.method == 'POST':
-        context = dict(cfg['public'])
-    
+    if request.method == "POST":
+        context = dict(cfg["public"])
+
     print(f"context: {context}")
-    return render_template('index.html', context=context)
+    return render_template("index.html", context=context)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
